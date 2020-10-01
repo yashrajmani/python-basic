2e54c5d64e77d1d7947cf9414978ba347fdad8b4
Yash Raj Mani <72155985+yashrajmani@users.noreply.github.com>
2020-10-02 04:13:20 +0530

    Add files via upload

---
+++ blocks.py
@@ -0,0 +1,7 @@
+for i in range(1,13):
+     print("Number{0} is squared is {1} cubed is {2:4}".format(i,i**2,i**3))
+     print("*" * 80)    #part of index
+print("this is out of  the index")
+print("*" * 80)         #not part of index
+
+
